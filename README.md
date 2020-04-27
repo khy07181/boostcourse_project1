7981f5c8885dee7d34ee16e6a3c8da2c94414512
Kimhayoung <khy07181@gmail.com>
2020-04-27 19:36:50 +0900

    project upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# boostcourse_project1
+# boostcourse_project1 - 나를 소개하는 페이지 만들기
+> [[부스트코스] 웹프로그래밍](https://www.edwith.org/boostcourse-web/joinLectures/12943)
---
+++ aboutme/src/TodayServlet.java
@@ -0,0 +1,60 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TodayServlet
+ */
+@WebServlet("/today")
+public class TodayServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TodayServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+		
+		String currentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/M/d HH:mm"));
+		
+		PrintWriter out = response.getWriter();
+		
+		out.print("<h3><a href=\"index.html\">메인화면</a>");
+		
+		out.print("<br><br><br>");
+		
+		out.print("<font size=\"20\">");
+		out.print("<center>");
+		out.print("현재시간 : " + currentTime);
+		out.print("</center>");
+		out.print("</font>");
+	}
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
